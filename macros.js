71032b34fc866cfcb149e9764ec9d21c06be1486
ajtmccarty <aaron.mccarty@packetfabric.com>
2021-02-15 06:59:55 -0500

    Organize macros.js more cleanly

--- macros.js
+++ macros.js
@@ -1,30 +1,6 @@
-function onOpen() {
-  var ui = SpreadsheetApp.getUi();
-  //  addColumns([DELIVERY_COLUMNS.latitude, DELIVERY_COLUMNS.longitude, DELIVERY_COLUMNS.cluster], true);
-  ui.alert(
-    'READ THIS MESSAGE BEFORE EDITING! \n There is a new AUTOMATED data system that sorts data into the DELIVERY tab when their STATUS is changed. \n NEEDS URGENT is no longer a status, it is a check box you will find in the row. \n Please fill in ALL other information for an order before changing the status column. \n If you change status to "pending delivery", that row will automatically be copied into those tabs. \n However, any changes made to the row in the new intake form after that WILL NOT be reflected in the pickup or delivery tabs. \n That is why its important to change the status after filling in ALL other information. Otherwise, please go and update information in the other tabs as well. Thank you!'
-  );
-  ui.createMenu("Automation")
-    .addItem("Post-delivery automation", "startUpMessage")
-    .addToUi();
-  ui.createMenu("Delivery Clustering")
-    .addItem("Sort Delivery Rows by Priority", "prioritizeRows")
-    .addItem("Geocode Delivery Addresses", "geocode")
-    .addItem("Cluster First 45 Delivery Rows", "clusterAddresses")
-    .addToUi();
-}
-
-function startUpMessage() {
-  let ui = SpreadsheetApp.getUi();
-  let buttonPressed = ui.alert(
-    "Please update status for all deliveries in DD sheet prior. Begin DD automation?",
-    ui.ButtonSet.YES_NO
-  );
-  if (buttonPressed == ui.Button.YES) {
-    ddAutomation();
-  }
-}
-
+/*
+ CONSTANTS START
+*/
 // URL for AWS Lambda clustering service
 const CLUSTERING_SERVICE_URL =
   "https://6t2mrznt84.execute-api.us-east-2.amazonaws.com/default/clusterAddresses";
@@ -61,11 +37,94 @@ const STATUS = {
   delivered: "Delivered",
   pendingDelivery: "Pending - Delivery",
 };
+/*
+ CONSTANTS END
+*/
 
-/**
- * Handle edits to sheets.
- */
+/*
+ UTILITIES START
+*/
+function getHeaderRow(sheetName) {
+  var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(sheetName);
+  return sheet.getRange(1, 1, 1, sheet.getLastColumn()).getValues()[0];
+}
+
+function getColumnIdx(columnName, sheetName = null) {
+  // Returns 0-indexed column index
+  if (sheetName === null) {
+    sheetName = SHEET.deliveries;
+  }
+  const idx = getHeaderRow(sheetName).indexOf(columnName);
+  // If no UID column, assume it is the first column
+  if (idx == -1 && columnName == DELIVERY_COLUMNS.uid) {
+    return 0;
+  }
+  return idx;
+}
+
+function addColumns(columnNames, hidden = false) {
+  var headers = getHeaderRow();
+  var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(
+    SHEET.deliveries
+  );
+  var lastColIdx = sheet.getLastColumn();
+  for (var i = 0; i < columnNames.length; i++) {
+    const name = columnNames[i];
+    if (!headers.includes(name)) {
+      sheet.insertColumnAfter(lastColIdx + i + 1);
+      var range = sheet.getRange(1, lastColIdx + i + 1);
+      range.setValue(name);
+      if (hidden) {
+        sheet.hideColumns(lastColIdx + i + 1);
+      }
+    }
+  }
+}
+/*
+ UTILITIES END
+*/
+
+/*
+ onOpen TRIGGER START
+*/
+function onOpen() {
+  var ui = SpreadsheetApp.getUi();
+  //  addColumns([DELIVERY_COLUMNS.latitude, DELIVERY_COLUMNS.longitude, DELIVERY_COLUMNS.cluster], true);
+  ui.alert(
+    'READ THIS MESSAGE BEFORE EDITING! \n There is a new AUTOMATED data system that sorts data into the DELIVERY tab when their STATUS is changed. \n NEEDS URGENT is no longer a status, it is a check box you will find in the row. \n Please fill in ALL other information for an order before changing the status column. \n If you change status to "pending delivery", that row will automatically be copied into those tabs. \n However, any changes made to the row in the new intake form after that WILL NOT be reflected in the pickup or delivery tabs. \n That is why its important to change the status after filling in ALL other information. Otherwise, please go and update information in the other tabs as well. Thank you!'
+  );
+  ui.createMenu("Automation")
+    .addItem("Post-delivery automation", "startUpMessage")
+    .addToUi();
+  ui.createMenu("Delivery Clustering")
+    .addItem("Sort Delivery Rows by Priority", "prioritizeRows")
+    .addItem("Geocode Delivery Addresses", "geocode")
+    .addItem("Cluster First 45 Delivery Rows", "clusterAddresses")
+    .addToUi();
+}
+
+function startUpMessage() {
+  let ui = SpreadsheetApp.getUi();
+  let buttonPressed = ui.alert(
+    "Please update status for all deliveries in DD sheet prior. Begin DD automation?",
+    ui.ButtonSet.YES_NO
+  );
+  if (buttonPressed == ui.Button.YES) {
+    ddAutomation();
+  }
+}
+/*
+ onOpen TRIGGER END
+*/
+
+/*
+ onEdit TRIGGER START
+*/
 function onEdit(event) {
+  statusChangeAutomation(event);
+}
+
+function statusChangeAutomation(event) {
   const sheet = event.source.getActiveSheet();
   const cell = sheet.getActiveCell();
   const cellR = cell.getRow();
@@ -99,12 +158,12 @@ function onEdit(event) {
   if (sheet.getName() == SHEET.intakeForm) {
     //if new intake status pending delivery or needs urgent will be moved to DD sheet
     if (cellValue == STATUS.pendingDelivery) {
-      cell.setValue("Scheduled Delivery");
       SpreadsheetApp.getActiveSpreadsheet()
         .getSheetByName(SHEET.deliveries)
         .appendRow(
           sheet.getRange(cellR, 1, 1, sheet.getLastColumn()).getValues()[0]
         );
+      cell.setValue("Scheduled Delivery");
       return;
     }
 
@@ -124,177 +183,23 @@ function onEdit(event) {
     }
   }
 }
+/*
+ onEdit TRIGGER END
+*/
 
-//if DD sheet status is set to delivered, changes status to delivered in New Intake and thus triggers moving it to the closed/completed tab
-//if(sheet.getName() == SHEET.deliveries){
-//  //if(cellC == getColIndex(DELIVERY_COLUMNS.status)){
-//    if(cellValue == STATUS.delivered){
-//     for(y=1; y < SpreadsheetApp.getActiveSpreadsheet().getSheetByName(SHEET.intakeForm).getLastRow(); y++){
-//       if(sheet.getRange(cellR, 1).getValue() == SpreadsheetApp.getActiveSpreadsheet().getSheetByName(SHEET.intakeForm).getRange(y, 1).getValue()){
-//        SpreadsheetApp.getActiveSpreadsheet().getSheetByName(SHEET.intakeForm).getRange(y, getColIndex(DELIVERY_COLUMNS.status)).setValue('Delivered');
-//        SpreadsheetApp.getActiveSpreadsheet().getSheetByName(SHEET.intakeForm).getRange(y, getColIndex(DELIVERY_COLUMNS.dateCompleted)).setValue(new Date());
-//        SpreadsheetApp.getActiveSpreadsheet().getSheetByName(SHEET.closedCompleted).appendRow(SpreadsheetApp.getActiveSpreadsheet().getSheetByName(SHEET.intakeForm).getRange(y, 1, 1, 50).getValues()[0]);
-//        SpreadsheetApp.getActiveSpreadsheet().getSheetByName(SHEET.intakeForm).deleteRow(y);
-//        sheet.deleteRow(cellR);
-//       }
-//    }
-//}
-//}
-
-//
-//
-function ddAutomation() {
-  var ui = SpreadsheetApp.getUi();
-  let now = new Date();
-  let ddRows = [];
-  let ddRows2 = [];
-  //  let ddRows3 = [];
-  const spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
-  //  const cell = sheet.getActiveCell();
-  //  const cellR = cell.getRow();
-  //  const cellC = cell.getColumn();
-  //  const cellValue = cell.getValue();
-
-  //if DD sheet status is set to delivered, changes status to delivered in New Intake, deletes from NI and D tab and copies to closed/completed
-  for (
-    var d = 1;
-    d < spreadsheet.getSheetByName(SHEET.deliveries).getLastRow();
-    d++
-  ) {
-    if (spreadsheet.getName() == SHEET.deliveries) {
-      if (spreadsheet.getRange(d, 3) == STATUS.delivered) {
-        ddRows.push(
-          spreadsheet.getSheetByName(SHEET.deliveries).getRange(d, 1).getValue()
-        );
-        ddRows2.push(d);
-        // the [0] bit is changing a Matrix to a list because getRange().getValues returns a section of the sheet not a row
-        spreadsheet
-          .getSheetByName(SHEET.closedCompleted)
-          .appendRow(
-            spreadsheet
-              .getSheetByName(SHEET.deliveries)
-              .getRange(
-                d,
-                1,
-                1,
-                spreadsheet.getSheetByName(SHEET.deliveries).getLastColumn()
-              )
-              .getValues()[0]
-          );
-        spreadsheet.getSheetByName(SHEET.deliveries).deleteRow(d);
-      }
-    }
-
-    ui.alert(`UIDs: ${ddRows} \n Row numbers: ${ddRows2}`);
-  }
-
-  for (
-    var n = 1;
-    n < spreadsheet.getsheetbyname(SHEET.intakeForm).getLastRow();
-    n++
-  ) {
-    for (let deliveredUID of ddRows) {
-      if (
-        spreadsheet
-          .getSheetByName(SHEET.intakeForm)
-          .getRange(n, 1)
-          .getValue() == deliveredUID
-      ) {
-        spreadsheet.getSheetByName(SHEET.intakeForm).deleteRow(n);
-      }
-    }
-  }
-
-  // Delete rows from the bottom up so that you don't change row indices as you're iterating
-  // Caution, reverse() changes the actual contents of rowNumsToDelete!
-  //deletes the rows from the SSNI sheet that are closed/completed and moved to that tab
-  for (let rowNum of rowNumsToDelete.reverse()) {
-    ssNI.deleteRow(rowNum);
-  }
-
-  for (let deliveredRow of ddrows2.reverse()) {
-    spreadsheet.getSheetByName(Deliveries).deleteRow(deliveredRow);
-  }
-}
-
-//
-//
-//     for(y=1; y < SpreadsheetApp.getActiveSpreadsheet().getSheetByName(SHEET.intakeForm).getLastRow(); y++){
-//       if(sheet.getRange(cellR, 1).getValue() == SpreadsheetApp.getActiveSpreadsheet().getSheetByName(SHEET.intakeForm).getRange(y, 1).getValue()){
-//        SpreadsheetApp.getActiveSpreadsheet().getSheetByName(SHEET.intakeForm).getRange(y, getColIndex(DELIVERY_COLUMNS.status)).setValue('Delivered');
-//        SpreadsheetApp.getActiveSpreadsheet().getSheetByName(SHEET.intakeForm).getRange(y, getColIndex(DELIVERY_COLUMNS.dateCompleted)).setValue(new Date());
-//        SpreadsheetApp.getActiveSpreadsheet().getSheetByName(SHEET.closedCompleted).appendRow(SpreadsheetApp.getActiveSpreadsheet().getSheetByName(SHEET.intakeForm).getRange(y, 1, 1, 50).getValues()[0]);
-//        SpreadsheetApp.getActiveSpreadsheet().getSheetByName(SHEET.intakeForm).deleteRow(y);
-//        sheet.deleteRow(cellR);
-//       }
-//    }
-//}
-//}
-//
-//
-//  }
-//
-//  function processDelivered() {
-//    let rowNumsToDelete = [];
-//
-//    //if a row is marked delivered or closed in NI sheet it will move that entire row to the closed/completed tab and delete those rows
-//    for (j = 2; j < ssNI.getMaxRows(); j++){
-//      if(ssNI.getRange(j, niStatCol).getValue() == 'Delivered' || ssNI.getRange(j, niStatCol).getValue() == 'Closed'){
-//        // the [0] bit is changing a Matrix to a list because getRange().getValues returns a section of the sheet not a row
-//        ssCC.appendRow(ssNI.getRange(j, 1, 1, ssNI.getLastColumn()).getValues()[0]);
-//        rowNumsToDelete.push(j);
-//      }
-//    }
-//
-//    // Delete rows from the bottom up so that you don't change row indices as you're iterating
-//    // Caution, reverse() changes the actual contents of rowNumsToDelete!
-//    //deletes the rows from the SSNI sheet that are closed/completed and moved to that tab
-//    for (let rowNum of rowNumsToDelete.reverse()){
-//      ssNI.deleteRow(rowNum);
-//    }
-//  }
-
-function getHeaderRow(sheetName) {
-  var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(sheetName);
-  return sheet.getRange(1, 1, 1, sheet.getLastColumn()).getValues()[0];
-}
-
-function getColumnIdx(columnName, sheetName = null) {
-  if (sheetName === null) {
-    sheetName = SHEET.deliveries;
-  }
-  const idx = getHeaderRow(sheetName).indexOf(columnName);
-  // If no UID column, assume it is the first column
-  if (idx == -1 && columnName == DELIVERY_COLUMNS.uid) {
-    return 0;
-  }
-  return idx;
-}
-
-function addColumns(columnNames, hidden = false) {
-  var headers = getHeaderRow();
-  var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(
-    SHEET.deliveries
-  );
-  var lastColIdx = sheet.getLastColumn();
-  for (var i = 0; i < columnNames.length; i++) {
-    const name = columnNames[i];
-    if (!headers.includes(name)) {
-      sheet.insertColumnAfter(lastColIdx + i + 1);
-      var range = sheet.getRange(1, lastColIdx + i + 1);
-      range.setValue(name);
-      if (hidden) {
-        sheet.hideColumns(lastColIdx + i + 1);
-      }
-    }
-  }
-}
-
+/*
+ CLUSTERING FUNCTIONS START
+*/
 function prioritizeRows() {
   var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(
     SHEET.deliveries
   );
-  let range = sheet.getRange(2, 1, sheet.getLastRow() - 1, sheet.getLastColumn());
+  let range = sheet.getRange(
+    2,
+    1,
+    sheet.getLastRow() - 1,
+    sheet.getLastColumn()
+  );
   range.sort([
     { column: getColumnIdx(DELIVERY_COLUMNS.urgent) + 1, ascending: false },
     { column: getColumnIdx(DELIVERY_COLUMNS.date) + 1, ascending: true },
@@ -485,3 +390,141 @@ function updateRowsWithClusters(clusterData) {
   }
   range.setValues(sheetValues);
 }
+/*
+ CLUSTERING FUNCTIONS END
+*/
+
+/*
+  UNUSED AUTOMATION START
+*/
+//if DD sheet status is set to delivered, changes status to delivered in New Intake and thus triggers moving it to the closed/completed tab
+//if(sheet.getName() == SHEET.deliveries){
+//  //if(cellC == getColIndex(DELIVERY_COLUMNS.status)){
+//    if(cellValue == STATUS.delivered){
+//     for(y=1; y < SpreadsheetApp.getActiveSpreadsheet().getSheetByName(SHEET.intakeForm).getLastRow(); y++){
+//       if(sheet.getRange(cellR, 1).getValue() == SpreadsheetApp.getActiveSpreadsheet().getSheetByName(SHEET.intakeForm).getRange(y, 1).getValue()){
+//        SpreadsheetApp.getActiveSpreadsheet().getSheetByName(SHEET.intakeForm).getRange(y, getColIndex(DELIVERY_COLUMNS.status)).setValue('Delivered');
+//        SpreadsheetApp.getActiveSpreadsheet().getSheetByName(SHEET.intakeForm).getRange(y, getColIndex(DELIVERY_COLUMNS.dateCompleted)).setValue(new Date());
+//        SpreadsheetApp.getActiveSpreadsheet().getSheetByName(SHEET.closedCompleted).appendRow(SpreadsheetApp.getActiveSpreadsheet().getSheetByName(SHEET.intakeForm).getRange(y, 1, 1, 50).getValues()[0]);
+//        SpreadsheetApp.getActiveSpreadsheet().getSheetByName(SHEET.intakeForm).deleteRow(y);
+//        sheet.deleteRow(cellR);
+//       }
+//    }
+//}
+//}
+
+//
+//
+function ddAutomation() {
+  var ui = SpreadsheetApp.getUi();
+  let now = new Date();
+  let ddRows = [];
+  let ddRows2 = [];
+  //  let ddRows3 = [];
+  const spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
+  //  const cell = sheet.getActiveCell();
+  //  const cellR = cell.getRow();
+  //  const cellC = cell.getColumn();
+  //  const cellValue = cell.getValue();
+
+  //if DD sheet status is set to delivered, changes status to delivered in New Intake, deletes from NI and D tab and copies to closed/completed
+  for (
+    var d = 1;
+    d < spreadsheet.getSheetByName(SHEET.deliveries).getLastRow();
+    d++
+  ) {
+    if (spreadsheet.getName() == SHEET.deliveries) {
+      if (spreadsheet.getRange(d, 3) == STATUS.delivered) {
+        ddRows.push(
+          spreadsheet.getSheetByName(SHEET.deliveries).getRange(d, 1).getValue()
+        );
+        ddRows2.push(d);
+        // the [0] bit is changing a Matrix to a list because getRange().getValues returns a section of the sheet not a row
+        spreadsheet
+          .getSheetByName(SHEET.closedCompleted)
+          .appendRow(
+            spreadsheet
+              .getSheetByName(SHEET.deliveries)
+              .getRange(
+                d,
+                1,
+                1,
+                spreadsheet.getSheetByName(SHEET.deliveries).getLastColumn()
+              )
+              .getValues()[0]
+          );
+        spreadsheet.getSheetByName(SHEET.deliveries).deleteRow(d);
+      }
+    }
+
+    ui.alert(`UIDs: ${ddRows} \n Row numbers: ${ddRows2}`);
+  }
+
+  for (
+    var n = 1;
+    n < spreadsheet.getsheetbyname(SHEET.intakeForm).getLastRow();
+    n++
+  ) {
+    for (let deliveredUID of ddRows) {
+      if (
+        spreadsheet
+          .getSheetByName(SHEET.intakeForm)
+          .getRange(n, 1)
+          .getValue() == deliveredUID
+      ) {
+        spreadsheet.getSheetByName(SHEET.intakeForm).deleteRow(n);
+      }
+    }
+  }
+
+  // Delete rows from the bottom up so that you don't change row indices as you're iterating
+  // Caution, reverse() changes the actual contents of rowNumsToDelete!
+  //deletes the rows from the SSNI sheet that are closed/completed and moved to that tab
+  for (let rowNum of rowNumsToDelete.reverse()) {
+    ssNI.deleteRow(rowNum);
+  }
+
+  for (let deliveredRow of ddrows2.reverse()) {
+    spreadsheet.getSheetByName(Deliveries).deleteRow(deliveredRow);
+  }
+}
+
+//
+//
+//     for(y=1; y < SpreadsheetApp.getActiveSpreadsheet().getSheetByName(SHEET.intakeForm).getLastRow(); y++){
+//       if(sheet.getRange(cellR, 1).getValue() == SpreadsheetApp.getActiveSpreadsheet().getSheetByName(SHEET.intakeForm).getRange(y, 1).getValue()){
+//        SpreadsheetApp.getActiveSpreadsheet().getSheetByName(SHEET.intakeForm).getRange(y, getColIndex(DELIVERY_COLUMNS.status)).setValue('Delivered');
+//        SpreadsheetApp.getActiveSpreadsheet().getSheetByName(SHEET.intakeForm).getRange(y, getColIndex(DELIVERY_COLUMNS.dateCompleted)).setValue(new Date());
+//        SpreadsheetApp.getActiveSpreadsheet().getSheetByName(SHEET.closedCompleted).appendRow(SpreadsheetApp.getActiveSpreadsheet().getSheetByName(SHEET.intakeForm).getRange(y, 1, 1, 50).getValues()[0]);
+//        SpreadsheetApp.getActiveSpreadsheet().getSheetByName(SHEET.intakeForm).deleteRow(y);
+//        sheet.deleteRow(cellR);
+//       }
+//    }
+//}
+//}
+//
+//
+//  }
+//
+//  function processDelivered() {
+//    let rowNumsToDelete = [];
+//
+//    //if a row is marked delivered or closed in NI sheet it will move that entire row to the closed/completed tab and delete those rows
+//    for (j = 2; j < ssNI.getMaxRows(); j++){
+//      if(ssNI.getRange(j, niStatCol).getValue() == 'Delivered' || ssNI.getRange(j, niStatCol).getValue() == 'Closed'){
+//        // the [0] bit is changing a Matrix to a list because getRange().getValues returns a section of the sheet not a row
+//        ssCC.appendRow(ssNI.getRange(j, 1, 1, ssNI.getLastColumn()).getValues()[0]);
+//        rowNumsToDelete.push(j);
+//      }
+//    }
+//
+//    // Delete rows from the bottom up so that you don't change row indices as you're iterating
+//    // Caution, reverse() changes the actual contents of rowNumsToDelete!
+//    //deletes the rows from the SSNI sheet that are closed/completed and moved to that tab
+//    for (let rowNum of rowNumsToDelete.reverse()){
+//      ssNI.deleteRow(rowNum);
+//    }
+//  }
+/*
+ UNUSED AUTOMATION END
+*/
